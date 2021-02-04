cdf0e3cdef16081005d22af86f5dcf981b3f6f2d
Tim Calas <timcalas@gmail.com>
2021-02-04 14:51:48 -0800

    add trivia api code

--- api-trivia/js/main.js
+++ api-trivia/js/main.js
@@ -1,25 +1,8 @@
-//Example fetch using pokemonapi.co
-// document.querySelector('button').addEventListener('click', getFetch)
-
-// function getFetch(){
-//   const choice = document.querySelector('input').value
-//   const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
-
-//   fetch(url)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         console.log(data)
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
-
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('#questionButton').addEventListener('click', getFetch)
+document.querySelector('#answerButton').addEventListener('click', showAnswer)
 
 function getFetch(){
   // const choice = document.querySelector('input').value
-  // const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
   const url = 'https://opentdb.com/api.php?amount=10'
 
 
@@ -27,9 +10,9 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        // document.querySelector('#questionHeader').innerText = data.
-        document.querySelector('#answerHeader').innerText = data.correct_answer
-        answer = 
+        document.querySelector('#questionHeader').innerText = data.results[0].question
+        // document.querySelector('#answerHeader').innerText = data.results[0].correct_answer
+        answer = data.results[0].correct_answer
       })
       .catch(err => {
           console.log(`error ${err}`)
@@ -37,5 +20,5 @@ function getFetch(){
 }
 
 function showAnswer() {
-  document.querySelector()
+  document.querySelector('#answerHeader').innerText = answer
 }
